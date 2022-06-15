5f5011e2713fe518791c020b31163b5d1a8aca6b
isaacscardosoblues <isaacscardosoblue@outlook.com>
2022-06-15 00:50:36 -0300

    Project created

---
+++ android/app/src/main/kotlin/com/example/expenses_diary/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.expenses_diary
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Expenses Diary',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue[200],
+    );
+  }
+}
