28bb0364af44db81df3e8b9d58a239db5692c70e
isaacscardosoblues <isaacscardosoblue@outlook.com>
2022-06-16 17:57:21 -0300

    Raising a Transaction classe, and stylizing to homepage.

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:expenses_diary/models/transaction.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(const MyApp());
@@ -12,13 +13,28 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatelessWidget {
-  const MyHomePage({Key? key}) : super(key: key);
+  MyHomePage({Key? key}) : super(key: key);
+
+  final _transactions = [
+    Transaction(
+      id: '1',
+      title: 'Bota Coturno',
+      value: 160.00,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: '2',
+      title: 'iPhone 13 Pro Max',
+      value: 9500.00,
+      date: DateTime.now(),
+    ),
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -26,6 +42,48 @@ class MyHomePage extends StatelessWidget {
       backgroundColor: Colors.blue[200],
       appBar: AppBar(
         title: const Text('Despesas Diárias'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          const Card(
+            child: Text(
+              'Gráfico',
+              textAlign: TextAlign.center,
+            ),
+          ),
+          Column(
+            children: _transactions.map((item) {
+              return Card(
+                child: Row(
+                  children: <Widget>[
+                    Container(
+                      margin: const EdgeInsets.symmetric(
+                        horizontal: 15,
+                        vertical: 10,
+                      ),
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: Colors.blueAccent,
+                          width: 2,
+                        ),
+                      ),
+                      padding: const EdgeInsets.all(10),
+                      child: Text(item.value.toString()),
+                    ),
+                    Column(
+                      children: <Widget>[
+                        Text(item.date.toString()),
+                        Text(item.title),
+                      ],
+                    ),
+                  ],
+                ),
+              );
+            }).toList(),
+          )
+        ],
       ),
     );
   }
