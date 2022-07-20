b913a1d488252de9062226778fc417501c2b5ba9
isaacscardoso <isaacscardosoblue@outlook.com>
2022-07-20 02:53:13 -0300

    Changing from, 'Card' built manually to list the items, to the 'Card' with 'ListTile.

--- lib/components/transaction_list.dart
+++ lib/components/transaction_list.dart
@@ -11,80 +11,66 @@ class TransactionList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final screenSize = MediaQuery.of(context).size.height;
-    return SizedBox(
-      height: screenSize - (screenSize * (27.8 / 100)),
-      child: transactions.isEmpty
-          ? Column(
-              children: <Widget>[
-                Padding(
-                  padding: const EdgeInsets.only(
-                    top: 12,
-                    bottom: 20,
+    return SingleChildScrollView(
+      child: SizedBox(
+        height: screenSize - (screenSize * (49.0 / 100)),
+        child: transactions.isEmpty
+            ? Column(
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.only(top: 12, bottom: 20),
+                    child: Text(
+                      'Nenhuma Transação cadastrada!',
+                      style: Theme.of(context).textTheme.headline6,
+                    ),
                   ),
-                  child: Text(
-                    'Nenhuma Transação cadastrada!',
-                    style: Theme.of(context).textTheme.headline6,
+                  SizedBox(
+                    height: screenSize - (screenSize * (66.7 / 100)),
+                    child: Image.asset(
+                      'assets/images/waiting.png',
+                      fit: BoxFit.cover,
+                    ),
                   ),
-                ),
-                SizedBox(
-                  height: screenSize - (screenSize * (66.7 / 100)),
-                  child: Image.asset(
-                    'assets/images/waiting.png',
-                    fit: BoxFit.cover,
-                  ),
-                ),
-              ],
-            )
-          : ListView.builder(
-              itemCount: transactions.length,
-              itemBuilder: (BuildContext context, int index) {
-                final item = transactions[index];
-                return Card(
-                  child: Row(
-                    children: <Widget>[
-                      Container(
-                        margin: const EdgeInsets.symmetric(
-                          horizontal: 15,
-                          vertical: 10,
-                        ),
-                        decoration: BoxDecoration(
-                          border: Border.all(
-                            color: Theme.of(context).colorScheme.primary,
-                            width: 2,
+                ],
+              )
+            : ListView.builder(
+                itemCount: transactions.length,
+                itemBuilder: (BuildContext context, int index) {
+                  final item = transactions[index];
+                  return Card(
+                    child: ListTile(
+                      leading: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: <Widget>[
+                          const Icon(Icons.payments_outlined),
+                          const SizedBox(
+                            height: 4,
                           ),
-                        ),
-                        padding: const EdgeInsets.all(10),
-                        child: Text(
-                          'R\$ ${item.value.toStringAsFixed(2)}',
-                          style: TextStyle(
-                            color: Theme.of(context).colorScheme.primary,
-                            fontWeight: FontWeight.w600,
+                          Text(
+                            'R\$ ${item.value.toStringAsFixed(2)}',
+                            style: TextStyle(
+                              color: Theme.of(context).colorScheme.primary,
+                              fontWeight: FontWeight.w600,
+                            ),
                           ),
+                        ],
+                      ),
+                      title: Text(
+                        DateFormat('d MMM y - HH:mm').format(item.date),
+                        style: const TextStyle(
+                          color: Colors.grey,
+                          fontSize: 12,
                         ),
                       ),
-                      Flexible(
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: <Widget>[
-                            Text(
-                              DateFormat('d MMM y - HH:mm').format(item.date),
-                              style: const TextStyle(
-                                color: Colors.grey,
-                                fontSize: 12,
-                              ),
-                            ),
-                            Text(
-                              item.title,
-                              style: Theme.of(context).textTheme.headline6,
-                            ),
-                          ],
-                        ),
+                      subtitle: Text(
+                        item.title,
+                        style: Theme.of(context).textTheme.headline6,
                       ),
-                    ],
-                  ),
-                );
-              },
-            ),
+                    ),
+                  );
+                },
+              ),
+      ),
     );
   }
 }
