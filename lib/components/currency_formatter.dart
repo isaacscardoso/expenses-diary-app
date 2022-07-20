5f542c36497a2c95a1bc04e6a8d5afbca5ce0743
isaacscardoso <isaacscardosoblue@outlook.com>
2022-07-20 20:03:25 -0300

    Implementation of the formatter and 'InputFormatter' for the Brazilian currency.

--- lib/components/currency_formatter.dart
+++
@@ -1,7 +0,0 @@
-import 'package:intl/intl.dart';
-
-class CurrencyFormatter {
-  static String currencyToBrl(double value) {
-    return NumberFormat.simpleCurrency(locale: 'pt_BR').format(value);
-  }
-}
